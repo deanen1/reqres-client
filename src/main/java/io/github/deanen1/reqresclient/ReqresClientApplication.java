b2075ec04d48645b75c762cff1b8f3c879e08f86
Norm Deane <norm.deane@vumc.org>
2024-04-04 16:58:45 -0500

    Added some basic error handling

--- src/main/java/io/github/deanen1/reqresclient/ReqresClientApplication.java
+++ src/main/java/io/github/deanen1/reqresclient/ReqresClientApplication.java
@@ -4,6 +4,7 @@ import java.util.List;
 import io.github.deanen1.reqresclient.service.reqres.User;
 import io.github.deanen1.reqresclient.service.reqres.UserClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,19 +14,29 @@ public class ReqresClientApplication implements CommandLineRunner {
   @Autowired
   private UserClient userClient;
 
+  @Value("${reqres.users.url}")
+  private String userServiceUrl;
+
   public static void main(String[] args) {
     SpringApplication.run(ReqresClientApplication.class, args);
   }
 
   @Override
   public void run(String... args) throws Exception {
-    List<User> users = userClient.getUsers(1);
+    try {
+      List<User> users = userClient.getUsers(1);
 
-    System.out.println("*".repeat(43));
-    System.out.println("Users");
-    System.out.println("*".repeat(43));
-    users.stream()
-        .forEach(u -> print(u));
+      System.out.println("*".repeat(43));
+      System.out.println("Users");
+      System.out.println("*".repeat(43));
+
+      users.stream()
+          .forEach(u -> print(u));
+    } catch (Exception e) {
+      System.out.println("*".repeat(65));
+      System.out.println("There was a problem retrieving the users from " + userServiceUrl);
+      System.out.println("*".repeat(65));
+    }
   }
 
   private void print(User u) {
